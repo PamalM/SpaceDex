85c0b66b77b36592a71fa237173de7f1bbd31579
Pamal Mangat <pamal13mangat@gmail.com>
2020-01-28 13:49:09 -0800

    Created class to convert json to python objects.

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Hello World")
-# Repository Test.
+import json
+
+class Planets:
+
+    #Constructor method to initialize attributes.
+    def __init__(self, name, mass, planetType, radius, density, lengthOfYear, lengthOfDay, numberOfMoons, equatorialIncilination, meanOrbitVelocity, averageOrbitDistance, volume, escapeVelocity, surfaceGravity):
+        self.name = name
+        self.mass = mass
+        self.planetType = planetType
+        self.radius = radius
+        self.density = density
+        self.lengthOfYear = lengthOfYear
+        self.lengthOfDay = lengthOfDay
+        self.numberOfMoons = numberOfMoons
+        self.equatorialIncilination = equatorialIncilination
+        self.meanOrbitVelocity = meanOrbitVelocity
+        self.averageOrbitDistance = averageOrbitDistance
+        self.volume = volume
+        self.escapeVelocity = escapeVelocity
+        self.surfaceGravity = surfaceGravity
+
